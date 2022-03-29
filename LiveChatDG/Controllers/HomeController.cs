ae6dab39d6902c824b5a6e2b6a83af250e0d3fa3
RuanCF <darkcraft399@gmail.com>
2022-03-29 18:29:44 -0300

    Update #4

--- LiveChatDG/Controllers/HomeController.cs
+++ LiveChatDG/Controllers/HomeController.cs
@@ -3,10 +3,7 @@ using LiveChatDG.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 //using Microsoft.EntityFrameworkCore;
 
--- LiveChatDG/Startup.cs
+++ LiveChatDG/Startup.cs
@@ -1,18 +1,12 @@
 using LiveChatDG.Data;
 using LiveChatDG.Hubs;
+using LiveChatDG.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LiveChatDG
 {
@@ -33,7 +27,7 @@ namespace LiveChatDG
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
 
